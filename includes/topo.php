3f70df57066f6adcd8b00b0b52b8b42c724258d3
gi <giovanah.ps@gmail.com>
2024-09-26 11:25:25 -0300

    acabei

--- includes/topo.php
+++ includes/topo.php
@@ -4,7 +4,7 @@
 
 <div class="card">
     <div class="card-header">
-        <h1> Projeto Blog em PHP - Silver Chavalier </h1>
+        <h1> Projeto Blog em PHP - É NOISEEEE </h1>
     </div>
     <?php if (isset($_SESSION['login'])): ?>
     <div class="card-body text-right">
